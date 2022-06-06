8d719a7f14db65b2fbc8ce65a5d37ebe86c65be6
ese111 <feung94@gmail.com>
2022-06-06 15:53:22 +0900

    feat: 예약 페이지 구현

---
+++ Android/app/src/main/java/com/team16/airbnb/ui/mybook/MyBookAdapter.kt
@@ -0,0 +1,52 @@
+package com.team16.airbnb.ui.mybook
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.team16.airbnb.data.model.MyBookData
+import com.team16.airbnb.databinding.ItemMyBookBinding
+import com.team16.airbnb.databinding.ItemWishBinding
+
+class MyBookAdapter :
+    ListAdapter<MyBookData.Result, MyBookAdapter.MyBookViewHolder>(MyBookDiffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyBookViewHolder {
+        return MyBookViewHolder(
+            ItemMyBookBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyBookViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class MyBookViewHolder(private val binding: ItemMyBookBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: MyBookData.Result) {
+            binding.item = item
+        }
+    }
+
+    private object MyBookDiffUtil : DiffUtil.ItemCallback<MyBookData.Result>() {
+        override fun areItemsTheSame(
+            oldItem: MyBookData.Result,
+            newItem: MyBookData.Result
+        ): Boolean {
+            return oldItem.roomId == newItem.roomId
+        }
+
+        override fun areContentsTheSame(
+            oldItem: MyBookData.Result,
+            newItem: MyBookData.Result
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+}
