e6fe8832d987827e0e921e15cf9e1c4aa4e9dc28
rkolx <olx.rko.o@gmail.com>
2022-06-10 01:24:20 +0900

    feat: 예약상세페이지 조회
    - ReservationRepository: 예약id를 통해 해당하는 id의 상세 예약데이터를 조회한다
    - ReservatitionDeatailDto: 예약상세페이지에 필요한 데이터로만 구성한다
    - ReservationService, ReservationController 구현

--- BE/src/main/java/yanse/airbnb/service/ReservationService.java
+++ BE/src/main/java/yanse/airbnb/service/ReservationService.java
@@ -3,7 +3,11 @@ package yanse.airbnb.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import yanse.airbnb.domain.image.repository.RoomImageRepository;
+import yanse.airbnb.domain.reservation.Reservation;
 import yanse.airbnb.domain.reservation.repository.ReservationRepository;
+import yanse.airbnb.web.dto.image.RoomImageDto;
+import yanse.airbnb.web.dto.reservation.ReservationDetailDto;
 import yanse.airbnb.web.dto.reservation.ReservationDto;
 
 import java.util.List;
@@ -15,11 +19,21 @@ import java.util.stream.Collectors;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final RoomImageRepository roomImageRepository;
 
     public List<ReservationDto> findReservationList(Long id) {
         return reservationRepository.findAllByReservation(id)
                 .stream()
                 .map(ReservationDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public ReservationDetailDto findReservation(Long id) {
+        Reservation reservation = reservationRepository.findById(id).orElseThrow(RuntimeException::new);
+        List<RoomImageDto> images = roomImageRepository.findAllByRoomId(id)
+                .stream()
+                .map(RoomImageDto::new)
+                .collect(Collectors.toList());
+        return new ReservationDetailDto(reservation, images);
     }
 }
--- BE/src/main/java/yanse/airbnb/web/controller/ReservationController.java
+++ BE/src/main/java/yanse/airbnb/web/controller/ReservationController.java
@@ -1,11 +1,9 @@
 package yanse.airbnb.web.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import yanse.airbnb.service.ReservationService;
+import yanse.airbnb.web.dto.reservation.ReservationDetailDto;
 import yanse.airbnb.web.dto.reservation.ReservationDto;
 
 import java.util.List;
@@ -20,5 +18,10 @@ public class ReservationController {
     @GetMapping("my")
     public List<ReservationDto> reservationList(@RequestParam("memberId") Long memberId){
         return reservationService.findReservationList(memberId);
+    }
+
+    @GetMapping("my/{id}")
+    public ReservationDetailDto reservationDetail(@PathVariable("id") Long id){
+        return reservationService.findReservation(id);
     }
 }
